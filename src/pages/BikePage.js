1c5f9c5da196583c7b1c35cbf4779dc757e87f4e
Brent Budts <brent.budts@student.thomasmore.be>
2022-10-10 23:35:08 +0200

    navbar

---
+++ src/pages/BikePage.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+
+export function BikePage(){
+
+    return (
+        <div>test</div>
+    )
+}
---
+++ src/pages/Home.js
@@ -0,0 +1,8 @@
+
+export function Home(){
+    return(
+        <div>
+            <p>Huur hier u springkasteel of bierfiets</p>
+        </div>
+    )
+}
