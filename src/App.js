7987df250d5077479b868509ecd9858a9ba6c240
Brent Budts <brent.budts@student.thomasmore.be>
2022-10-11 14:15:41 +0200

    old app

--- src/App.js
+++ src/App.js
@@ -10,10 +10,27 @@ import NavDropdown from 'react-bootstrap/NavDropdown';
 import {Home} from "./pages/Home";
 
 function App() {
-    return (
-        <div>
-            <p>TEST</p>
-        </div>
+    return (<HashRouter>
+            <Navbar bg="light" expand="lg">
+                <Container>
+                    <Navbar.Brand h>Bar Kar</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav"/>
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="me-auto">
+                            <Nav.Link><Link to="Bierfiets/">Home</Link></Nav.Link>
+                            <NavDropdown title={<Link to="Bierfiets/attracties">Attracties</Link>} id="basic-nav-dropdown">
+                                <NavDropdown.Item></NavDropdown.Item>
+                            </NavDropdown>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+
+            <Routes>
+                <Route path="Bierfiets/" element={<Home/>}/>
+                <Route path="Bierfiets/attracties" element={<BikePage/>}/>
+            </Routes>
+        </HashRouter>
     );
 }
 
