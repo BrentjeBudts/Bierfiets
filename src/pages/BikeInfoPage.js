6f34c56fcc6bb46f5b2404fb5114726b487cfb1b
brent <brent.budts11@gmail.com>
2022-10-29 18:45:11 +0200

    update doc does not work

--- src/pages/BikeInfoPage.js
+++ src/pages/BikeInfoPage.js
@@ -1,6 +1,9 @@
 
 import {useParams} from "react-router-dom";
 import React, {useEffect, useState} from "react";
+import Button from "react-bootstrap/Button";
+import { doc, updateDoc } from "firebase/firestore";
+import {firestoreDB} from "../services/firestore";
 
 
 export function BikeInfoPage(props){
@@ -9,11 +12,13 @@ export function BikeInfoPage(props){
     const [bike, setBike] = useState({});
     useEffect(()=>{
         setBike(bikes.find(b=> id == b.id));
-    })
+    });
+    const docRef = doc(firestoreDB, "BeerBikes",bike);
     return(
         <div>
             <h4>{bike.name}</h4>
             <p>//TODO FORM</p>
+            <Button onClick={updateDoc(docRef,{hired:true})}></Button>
         </div>
     )
 }
