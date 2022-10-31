ced334b8322370a86d6f677690cb4608b59e495c
Brent Budts <brent.budts@student.thomasmore.be>
2022-10-31 20:10:17 +0100

    working firebase

--- src/pages/BikeInfoPage.js
+++ src/pages/BikeInfoPage.js
@@ -1,6 +1,9 @@
 
 import {useParams} from "react-router-dom";
 import React, {useEffect, useState} from "react";
+import {firestoreDB} from "../services/firestore";
+import { doc, updateDoc } from "firebase/firestore";
+import Button from "react-bootstrap/Button";
 
 
 export function BikeInfoPage(props){
@@ -10,10 +13,34 @@ export function BikeInfoPage(props){
     useEffect(()=>{
         setBike(bikes.find(b=> id == b.id));
     })
+
+    const updateBike =() =>{
+
+        updateDoc(bikes, bike).then(r => 0);
+    }
+
     return(
         <div>
             <h4>{bike.name}</h4>
             <p>//TODO FORM</p>
+            <Button onClick={updateBike}>HIRE</Button>
         </div>
     )
+}
+
+
+
+export const firestoreConverter = {
+    toFirestore: function(dataInApp) {
+        const dataInDb = {};
+        Object.entries(dataInApp).forEach(entry => {
+            const [key, value] = entry;
+            dataInDb[key] = value;
+        });
+        return dataInDb;
+    },
+    fromFirestore: function(snapshot, options) {
+        const data = snapshot.data(options);
+        return {...data, id: snapshot.id, ref: snapshot.ref};
+    }
 }
